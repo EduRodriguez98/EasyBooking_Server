47607f67be84447b6991e64c6ced36cdf9263371
EduRodriguez98 <55583447+EduRodriguez98@users.noreply.github.com>
2021-01-22 10:56:07 +0100

    Delete EasyBooking_Server directory
    Adding content the team worked on.

--- EasyBooking_Server/src/gateway/IGatewayAuthentication.java
+++
@@ -1,9 +0,0 @@
-package gateway;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IGatewayAuthentication extends Remote {
-	public boolean validateUser(String username, String password) throws RemoteException;
-	public boolean registerUser(String username, String password) throws RemoteException;
-}
--- EasyBooking_Server/src/gateway/IGatewayPayment.java
+++
@@ -1,9 +0,0 @@
-package gateway;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IGatewayPayment extends Remote {
-	
-	public void bookPayFlight(String username, String password, int flightNum, int numSeats) throws RemoteException;
-}
